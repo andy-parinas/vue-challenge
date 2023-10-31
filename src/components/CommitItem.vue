3d75e4418601d2e1bfb12de7ba3b4aa8c7627dad
budgetpetproducts <andy.parinas@gmail.com>
2023-10-31 11:44:03 +1000

    First Commit. Refactor codes

---
+++ src/components/CommitItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>{{truncate(sha)}}</div>
+    <div>{{commit.author.name}}</div>
+    <div>{{formatDate(commit.author.date)}}</div>
+    <div>{{commit.message}}</div>
+  </div>
+</template>
+
+
+<script>
+import moment from "moment/moment";
+
+export default {
+  name: "CommitItem",
+  props: ['commit', 'author', 'sha', 'html_url'],
+  methods: {
+    formatDate: function (date) {
+      const outputFormat = "DD/MM/YYYY h:mm:ss A";
+      return moment(date).format(outputFormat);
+    },
+    truncate: function (text) {
+      return text.slice(0, 7)
+    },
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/Commits.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container-fluid">
+    <div>Latest Commits</div>
+    <div>
+      <template v-for="{ html_url, sha, author, commit } in commits" :key="html_url" >
+        <CommitItem
+            :commit="commit"
+            :author="author"
+            :sha="sha"
+            :html_url="html_url" />
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+import CommitItem from "./CommitItem.vue";
+export default {
+  //name: 'component-name',
+  //parent: '',
+  components: {CommitItem},
+  directives: {},
+  filters: {},
+  mixins: [],
+  props: {},
+  data: function () {
+    return {
+      API_URL:
+        "https://api.github.com/repos/vuejs/core/commits?per_page=10&sha=main",
+      commits: null,
+    };
+  },
+  computed: {},
+  watch: {},
+  beforeCreate: function () {},
+  created: function () {
+    this.fetchData();
+  },
+  beforeMount: function () {},
+  mounted: function () {},
+  beforeUpdate: function () {},
+  updated: function () {},
+  activated: function () {},
+  deactivated: function () {},
+  beforeDestroy: function () {},
+  destroyed: function () {},
+  methods: {
+    fetchData: async function () {
+      this.commits = await (await fetch(this.API_URL)).json();
+      console.log(this.commits)
+    },
+
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
